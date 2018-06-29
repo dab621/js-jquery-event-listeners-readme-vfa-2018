7d89df43dad7108d77b3b1b07454ce3e0e9c73d1
Danielle Bennett <dab621@stern.nyu.edu>
2018-06-29 08:02:20 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,8 +4,8 @@ getIt = $('p').on("dblclick keypress", function(){
 });
 
 frameIt = $('.tasty').on('load',
-function(){'add red frame to image'
-};
+function(){'add red frame to image'}
+);
 pressIt = $("form").on('keydown', function(key) {
   if(key.which == 83){
       alert('G was pressed');
